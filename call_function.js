e8826931103aff9ac3b4b7e0e132395bef110ee3
AndriiKot <a_kocyuba@ukr.net>
2023-03-28 19:52:37 +0200

    let variables and functions

--- call_function.js
+++ call_function.js
@@ -1,8 +1,26 @@
-﻿const sum = (a = 0, b = 0) => {
+﻿//Variation 1
+const sum_v1 = (a = 0, b = 0) => {
  return a + b;
 };
 
-sum(6,5)  // return 11
-sum(6)    // return 6
-sum()     // retunr 0
+sum_v1(6,5);   // return 11
+sum_v1(6);    // return 6
+sum_v1();    // retunr 0
+
+//Variation 2
+
+const sum_v2 = (a,b) => {
+  return a + b;
+};
+
+sum_v2(6,5);  // return 11
+sum_v2(5);    // return NaN
+sum_v2();     // return NaN
+
+//Variation 3
+
+const sum_v3 = (...args) => {
+  return args[0] = args[1];
+};
+
 
